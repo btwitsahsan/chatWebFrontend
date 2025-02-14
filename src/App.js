8cd618ea8638b800af77c529d2b9cfddb793517e
btwitsahsan <ahsan899289@gmail.com>
2025-02-14 19:14:45 +0500

    apply deployment api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import axios from "axios";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { checkLoginStatus, getUser } from "./redux/features/auth/authSlice";
@@ -16,46 +16,38 @@ import { ChildRoute } from "./components/hiddenLink/hiddenLink";
 import Users from "./pages/users/Users";
 
 function App() {
+  const { isLoggedIn } = useSelector((state) => state.auth);
 
-    const { isLoggedIn } = useSelector((state) => state.auth);
-  
   axios.defaults.withCredentials = true;
   const dispatch = useDispatch();
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(checkLoginStatus());
-  },[dispatch])
+  }, [dispatch]);
 
-   useEffect(() => {
-      if (isLoggedIn) {
-        dispatch(getUser());
-      }
-    }, [dispatch, isLoggedIn]);
+  useEffect(() => {
+    if (isLoggedIn) {
+      dispatch(getUser());
+    }
+  }, [dispatch, isLoggedIn]);
 
   return (
-    
     <BrowserRouter>
-    <ToastContainer/>
+      <ToastContainer />
       {/* <Loader/> */}
       <Header />
       {/* <Spinner/> */}
-      {/* <Routes element={<ChildRoute/>}>
-     
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/chat" element={ <PrivateRoute element={<Chat/>}/> } />
-        <Route path="/profile" element={ <PrivateRoute element={<Profile/>}/> } />
-  
-      </Routes> */}
       <Routes>
         {/* Define the parent route */}
         <Route path="/" element={<ChildRoute />}>
-          <Route index element={isLoggedIn? <Users />: <Login/>} />
-          <Route path="/login" element={<Login/>}/>
+          <Route index element={isLoggedIn ? <Users /> : <Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/chat" element={isLoggedIn? <Chat />: <Login/>}/>
-          <Route path="/profile" element={isLoggedIn?<Profile /> : <Login/>} />
+          <Route path="/chat" element={isLoggedIn ? <Chat /> : <Login />} />
+          <Route
+            path="/profile"
+            element={isLoggedIn ? <Profile /> : <Login />}
+          />
         </Route>
       </Routes>
       <Footer />
