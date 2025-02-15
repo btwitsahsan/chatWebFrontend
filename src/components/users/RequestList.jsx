f7754b8d53ff0955b3ab12b26218d83806dc7931
btwitsahsan <ahsan899289@gmail.com>
2025-02-15 15:08:03 +0500

    final look

--- src/components/users/RequestList.jsx
+++ src/components/users/RequestList.jsx
@@ -6,10 +6,14 @@ const RequestList = ({ requests, acceptRequest, rejectRequest }) => {
     <div className="request-list">
       {requests?.map(request => (
         <div key={request.senderId._id} className="request-item">
+          <div className='pic-na-div'>
           <img src={request.senderId.photo} alt={request.senderId.name} className="request-profile-pic" />
           <span className="request-name">{request.senderId.name}</span>
+          </div>
+          <div className='acc-rej-btn'>
           <button onClick={() => acceptRequest(request._id)} className="accept-button">Accept</button>
           <button onClick={() => rejectRequest(request._id)} className="reject-button">Reject</button>
+          </div>
         </div>
       ))}
     </div>
